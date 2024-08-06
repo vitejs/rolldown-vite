333a00b10803b91ad2c4b7c9f1224cf9b0df08d0
underfin <likui.underfin@gmail.com>
2024-08-06 18:08:40 +0800

    fix: css chunk meta

--- packages/vite/src/node/plugins/css.ts
+++ packages/vite/src/node/plugins/css.ts
@@ -930,7 +930,7 @@ export function cssPostPlugin(config: ResolvedConfig): Plugin {
             // chunks instead.
             chunk.imports = chunk.imports.filter((file) => {
               if (pureCssChunkNames.includes(file)) {
-                const { importedCss, importedAssets } =  getChunkMetadata((bundle[file] as OutputChunk).fileName)!
+                const { importedCss, importedAssets } =  getChunkMetadata((bundle[file] as OutputChunk).name)!
                 importedCss.forEach((file) =>
                   getChunkMetadata(chunk.name)!.importedCss.add(file),
                 )
