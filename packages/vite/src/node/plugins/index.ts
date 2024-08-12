46f22d81021d25c484ec0771067962e99dc43b02
underfin <likui.underfin@gmail.com>
2024-08-12 10:46:54 +0800

    chore: enable plugin order

--- packages/vite/src/node/plugins/index.ts
+++ packages/vite/src/node/plugins/index.ts
@@ -143,21 +143,21 @@ export function getSortedPluginsByHook<K extends keyof Plugin>(
   const sortedPlugins: Plugin[] = []
   // Use indexes to track and insert the ordered plugins directly in the
   // resulting array to avoid creating 3 extra temporary arrays per hook
-  const pre = 0;
-    let normal = 0;
-    // post = 0
+  let pre = 0,
+    normal = 0,
+    post = 0
   for (const plugin of plugins) {
     const hook = plugin[hookName]
     if (hook) {
       if (typeof hook === 'object') {
-        // if (hook.order === 'pre') {
-        //   sortedPlugins.splice(pre++, 0, plugin)
-        //   continue
-        // }
-        // if (hook.order === 'post') {
-        //   sortedPlugins.splice(pre + normal + post++, 0, plugin)
-        //   continue
-        // }
+        if (hook.order === 'pre') {
+          sortedPlugins.splice(pre++, 0, plugin)
+          continue
+        }
+        if (hook.order === 'post') {
+          sortedPlugins.splice(pre + normal + post++, 0, plugin)
+          continue
+        }
       }
       sortedPlugins.splice(pre + normal++, 0, plugin)
     }
