532b8c3e0b364afff0dd2c6a21cd49bc4fea80b3
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-09-11 19:56:17 +0900

    feat: use rolldown in the dep optimizer
    Co-authored-by: underfin <likui.underfin@gmail.com>

--- packages/vite/src/node/config.ts
+++ packages/vite/src/node/config.ts
@@ -690,6 +690,7 @@ export const configDefaults = Object.freeze({
     exclude: [],
     needsInterop: [],
     // esbuildOptions
+    rollupOptions: {},
     /** @experimental */
     extensions: [],
     /** @deprecated @experimental */
