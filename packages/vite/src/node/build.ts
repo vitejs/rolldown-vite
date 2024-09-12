632754e3a93bd802e8936ab30043ef7db4773f4e
IWANABETHATGUY <iwanabethatguy@qq.com>
2024-09-12 20:02:47 +0800

    perf: reduce pre alias plugin in build mode

--- packages/vite/src/node/build.ts
+++ packages/vite/src/node/build.ts
@@ -484,7 +484,7 @@ export async function resolveBuildPlugins(config: ResolvedConfig): Promise<{
     ],
     post: [
       ...buildImportAnalysisPlugin(config),
-      buildEsbuildPlugin(),
+      ...(!enableNativePlugin ? [buildEsbuildPlugin()] : []),
       terserPlugin(config),
       ...(!config.isWorker
         ? [
