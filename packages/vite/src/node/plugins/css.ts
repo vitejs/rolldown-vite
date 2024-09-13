8d2ce8f135db05cd8bf8846c5b01918f846fb9d4
IWANABETHATGUY <iwanabethatguy@qq.com>
2024-09-13 16:42:12 +0800

    test: 💍 css filterable

--- packages/vite/src/node/plugins/css.ts
+++ packages/vite/src/node/plugins/css.ts
@@ -412,7 +412,7 @@ export function cssPlugin(config: ResolvedConfig): RolldownPlugin {
 /**
  * Plugin applied after user plugins
  */
-export function cssPostPlugin(config: ResolvedConfig): Plugin {
+export function cssPostPlugin(config: ResolvedConfig): RolldownPlugin {
   // styles initialization in buildStart causes a styling loss in watch
   const styles: Map<string, string> = new Map<string, string>()
   // queue to emit css serially to guarantee the files are emitted in a deterministic order
@@ -460,115 +460,128 @@ export function cssPostPlugin(config: ResolvedConfig): Plugin {
       codeSplitEmitQueue = createSerialPromiseQueue()
     },
 
-    async transform(css, id, options) {
-      if (
-        !isCSSRequest(id) ||
-        commonjsProxyRE.test(id) ||
-        SPECIAL_QUERY_RE.test(id)
-      ) {
-        return
-      }
-
-      css = stripBomTag(css)
-
-      // cache css compile result to map
-      // and then use the cache replace inline-style-flag
-      // when `generateBundle` in vite:build-html plugin and devHtmlHook
-      const inlineCSS = inlineCSSRE.test(id)
-      const isHTMLProxy = htmlProxyRE.test(id)
-      if (inlineCSS && isHTMLProxy) {
-        if (styleAttrRE.test(id)) {
-          css = css.replace(/"/g, '&quot;')
+    transform: {
+      filter: {
+        id: {
+          include: [CSS_LANGS_RE],
+          exclude: [commonjsProxyRE, SPECIAL_QUERY_RE],
+        },
+      },
+      async handler(css, id, options) {
+        if (
+          !isCSSRequest(id) ||
+          commonjsProxyRE.test(id) ||
+          SPECIAL_QUERY_RE.test(id)
+        ) {
+          return
         }
-        const index = htmlProxyIndexRE.exec(id)?.[1]
-        if (index == null) {
-          throw new Error(`HTML proxy index in "${id}" not found`)
-        }
-        addToHTMLProxyTransformResult(
-          `${getHash(cleanUrl(id))}_${Number.parseInt(index)}`,
-          css,
-        )
-        return `export default ''`
-      }
 
-      const inlined = inlineRE.test(id)
-      const modules = cssModulesCache.get(config)!.get(id)
+        css = stripBomTag(css)
 
-      // #6984, #7552
-      // `foo.module.css` => modulesCode
-      // `foo.module.css?inline` => cssContent
-      const modulesCode =
-        modules &&
-        !inlined &&
-        dataToEsm(modules, { namedExports: true, preferConst: true })
-
-      if (config.command === 'serve') {
-        const getContentWithSourcemap = async (content: string) => {
-          if (config.css?.devSourcemap) {
-            // @ts-expect-error missing types
-            const sourcemap = this.getCombinedSourcemap()
-            if (sourcemap.mappings) {
-              await injectSourcesContent(sourcemap, cleanUrl(id), config.logger)
-            }
-            return getCodeWithSourcemap('css', content, sourcemap)
+        // cache css compile result to map
+        // and then use the cache replace inline-style-flag
+        // when `generateBundle` in vite:build-html plugin and devHtmlHook
+        const inlineCSS = inlineCSSRE.test(id)
+        const isHTMLProxy = htmlProxyRE.test(id)
+        if (inlineCSS && isHTMLProxy) {
+          if (styleAttrRE.test(id)) {
+            css = css.replace(/"/g, '&quot;')
           }
-          return content
+          const index = htmlProxyIndexRE.exec(id)?.[1]
+          if (index == null) {
+            throw new Error(`HTML proxy index in "${id}" not found`)
+          }
+          addToHTMLProxyTransformResult(
+            `${getHash(cleanUrl(id))}_${Number.parseInt(index)}`,
+            css,
+          )
+          return `export default ''`
         }
 
-        if (isDirectCSSRequest(id)) {
-          return null
+        const inlined = inlineRE.test(id)
+        const modules = cssModulesCache.get(config)!.get(id)
+
+        // #6984, #7552
+        // `foo.module.css` => modulesCode
+        // `foo.module.css?inline` => cssContent
+        const modulesCode =
+          modules &&
+          !inlined &&
+          dataToEsm(modules, { namedExports: true, preferConst: true })
+
+        if (config.command === 'serve') {
+          const getContentWithSourcemap = async (content: string) => {
+            if (config.css?.devSourcemap) {
+              // @ts-expect-error missing types
+              const sourcemap = this.getCombinedSourcemap()
+              if (sourcemap.mappings) {
+                await injectSourcesContent(
+                  sourcemap,
+                  cleanUrl(id),
+                  config.logger,
+                )
+              }
+              return getCodeWithSourcemap('css', content, sourcemap)
+            }
+            return content
+          }
+
+          if (isDirectCSSRequest(id)) {
+            return null
+          }
+          // server only
+          // @ts-expect-error TODO: rolldown plugin config compatible
+          if (options?.ssr) {
+            return modulesCode || `export default ${JSON.stringify(css)}`
+          }
+          if (inlined) {
+            return `export default ${JSON.stringify(css)}`
+          }
+
+          const cssContent = await getContentWithSourcemap(css)
+          const code = [
+            `import { updateStyle as __vite__updateStyle, removeStyle as __vite__removeStyle } from ${JSON.stringify(
+              path.posix.join(config.base, CLIENT_PUBLIC_PATH),
+            )}`,
+            `const __vite__id = ${JSON.stringify(id)}`,
+            `const __vite__css = ${JSON.stringify(cssContent)}`,
+            `__vite__updateStyle(__vite__id, __vite__css)`,
+            // css modules exports change on edit so it can't self accept
+            `${modulesCode || 'import.meta.hot.accept()'}`,
+            `import.meta.hot.prune(() => __vite__removeStyle(__vite__id))`,
+          ].join('\n')
+          return { code, map: { mappings: '' } }
         }
-        // server only
-        if (options?.ssr) {
-          return modulesCode || `export default ${JSON.stringify(css)}`
+
+        // build CSS handling ----------------------------------------------------
+
+        // record css
+        if (!inlined) {
+          styles.set(id, css)
         }
-        if (inlined) {
-          return `export default ${JSON.stringify(css)}`
+
+        let code: string
+        if (modulesCode) {
+          code = modulesCode
+        } else if (inlined) {
+          let content = css
+          if (config.build.cssMinify) {
+            content = await minifyCSS(content, config, true)
+          }
+          code = `export default ${JSON.stringify(content)}`
+        } else {
+          // empty module when it's not a CSS module nor `?inline`
+          code = ''
         }
 
-        const cssContent = await getContentWithSourcemap(css)
-        const code = [
-          `import { updateStyle as __vite__updateStyle, removeStyle as __vite__removeStyle } from ${JSON.stringify(
-            path.posix.join(config.base, CLIENT_PUBLIC_PATH),
-          )}`,
-          `const __vite__id = ${JSON.stringify(id)}`,
-          `const __vite__css = ${JSON.stringify(cssContent)}`,
-          `__vite__updateStyle(__vite__id, __vite__css)`,
-          // css modules exports change on edit so it can't self accept
-          `${modulesCode || 'import.meta.hot.accept()'}`,
-          `import.meta.hot.prune(() => __vite__removeStyle(__vite__id))`,
-        ].join('\n')
-        return { code, map: { mappings: '' } }
-      }
-
-      // build CSS handling ----------------------------------------------------
-
-      // record css
-      if (!inlined) {
-        styles.set(id, css)
-      }
-
-      let code: string
-      if (modulesCode) {
-        code = modulesCode
-      } else if (inlined) {
-        let content = css
-        if (config.build.cssMinify) {
-          content = await minifyCSS(content, config, true)
+        return {
+          code,
+          map: { mappings: '' },
+          // avoid the css module from being tree-shaken so that we can retrieve
+          // it in renderChunk()
+          moduleSideEffects: modulesCode || inlined ? false : 'no-treeshake',
         }
-        code = `export default ${JSON.stringify(content)}`
-      } else {
-        // empty module when it's not a CSS module nor `?inline`
-        code = ''
-      }
-
-      return {
-        code,
-        map: { mappings: '' },
-        // avoid the css module from being tree-shaken so that we can retrieve
-        // it in renderChunk()
-        moduleSideEffects: modulesCode || inlined ? false : 'no-treeshake',
-      }
+      },
     },
 
     async renderChunk(code, chunk, opts) {
@@ -978,7 +991,9 @@ export function cssPostPlugin(config: ResolvedConfig): Plugin {
   }
 }
 
-export function cssAnalysisPlugin(config: ResolvedConfig): Plugin {
+export function cssAnalysisPlugin(
+  config: ResolvedConfig,
+): RolldownPlugin & Plugin {
   let server: ViteDevServer
 
   return {
@@ -988,59 +1003,68 @@ export function cssAnalysisPlugin(config: ResolvedConfig): Plugin {
       server = _server
     },
 
-    async transform(_, id, options) {
-      if (
-        !isCSSRequest(id) ||
-        commonjsProxyRE.test(id) ||
-        SPECIAL_QUERY_RE.test(id)
-      ) {
-        return
-      }
-
-      const ssr = options?.ssr === true
-      const { moduleGraph } = server
-      const thisModule = moduleGraph.getModuleById(id)
-
-      // Handle CSS @import dependency HMR and other added modules via this.addWatchFile.
-      // JS-related HMR is handled in the import-analysis plugin.
-      if (thisModule) {
-        // CSS modules cannot self-accept since it exports values
-        const isSelfAccepting =
-          !cssModulesCache.get(config)?.get(id) &&
-          !inlineRE.test(id) &&
-          !htmlProxyRE.test(id)
-        // attached by pluginContainer.addWatchFile
-        const pluginImports = (this as unknown as TransformPluginContext)
-          ._addedImports
-        if (pluginImports) {
-          // record deps in the module graph so edits to @import css can trigger
-          // main import to hot update
-          const depModules = new Set<string | ModuleNode>()
-          for (const file of pluginImports) {
-            depModules.add(
-              isCSSRequest(file)
-                ? moduleGraph.createFileOnlyEntry(file)
-                : await moduleGraph.ensureEntryFromUrl(
-                    fileToDevUrl(file, config, /* skipBase */ true),
-                    ssr,
-                  ),
-            )
-          }
-          moduleGraph.updateModuleInfo(
-            thisModule,
-            depModules,
-            null,
-            // The root CSS proxy module is self-accepting and should not
-            // have an explicit accept list
-            new Set(),
-            null,
-            isSelfAccepting,
-            ssr,
-          )
-        } else {
-          thisModule.isSelfAccepting = isSelfAccepting
+    transform: {
+      filter: {
+        id: {
+          include: [CSS_LANGS_RE],
+          exclude: [commonjsProxyRE, SPECIAL_QUERY_RE],
+        },
+      },
+      // @ts-expect-error TODO: rolldown plugin config compatible
+      async handler(_, id, options) {
+        if (
+          !isCSSRequest(id) ||
+          commonjsProxyRE.test(id) ||
+          SPECIAL_QUERY_RE.test(id)
+        ) {
+          return
         }
-      }
+
+        const ssr = options?.ssr === true
+        const { moduleGraph } = server
+        const thisModule = moduleGraph.getModuleById(id)
+
+        // Handle CSS @import dependency HMR and other added modules via this.addWatchFile.
+        // JS-related HMR is handled in the import-analysis plugin.
+        if (thisModule) {
+          // CSS modules cannot self-accept since it exports values
+          const isSelfAccepting =
+            !cssModulesCache.get(config)?.get(id) &&
+            !inlineRE.test(id) &&
+            !htmlProxyRE.test(id)
+          // attached by pluginContainer.addWatchFile
+          const pluginImports = (this as unknown as TransformPluginContext)
+            ._addedImports
+          if (pluginImports) {
+            // record deps in the module graph so edits to @import css can trigger
+            // main import to hot update
+            const depModules = new Set<string | ModuleNode>()
+            for (const file of pluginImports) {
+              depModules.add(
+                isCSSRequest(file)
+                  ? moduleGraph.createFileOnlyEntry(file)
+                  : await moduleGraph.ensureEntryFromUrl(
+                      fileToDevUrl(file, config, /* skipBase */ true),
+                      ssr,
+                    ),
+              )
+            }
+            moduleGraph.updateModuleInfo(
+              thisModule,
+              depModules,
+              null,
+              // The root CSS proxy module is self-accepting and should not
+              // have an explicit accept list
+              new Set(),
+              null,
+              isSelfAccepting,
+              ssr,
+            )
+          } else {
+            thisModule.isSelfAccepting = isSelfAccepting
+          }
+        }
+      },
     },
   }
 }
