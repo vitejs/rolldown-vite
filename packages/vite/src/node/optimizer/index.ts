416bf50d7acc3368f7cc8e641a539992f773f642
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-09-24 13:47:21 +0900

    chore: bump rolldown

--- packages/vite/src/node/optimizer/index.ts
+++ packages/vite/src/node/optimizer/index.ts
@@ -781,6 +781,11 @@ async function prepareRolldownOptimizerRun(
         conditionNames: ['browser'],
       },
       ...rollupOptions,
+      // TODO: remove this and enable rolldown's CSS support later
+      moduleTypes: {
+        '.css': 'js',
+        ...rollupOptions.moduleTypes,
+      },
     })
     if (canceled) {
       await bundle.close()
@@ -1062,6 +1067,11 @@ export async function extractExportsData(
       ...remainingRollupOptions,
       plugins,
       input: [filePath],
+      // TODO: remove this and enable rolldown's CSS support later
+      moduleTypes: {
+        '.css': 'js',
+        ...remainingRollupOptions.moduleTypes,
+      },
     })
     const result = await build.generate({
       ...rollupOptions.output,
