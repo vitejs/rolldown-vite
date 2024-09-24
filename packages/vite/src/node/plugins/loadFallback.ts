fcb0c948718d32c868ef5ce106addd501ce89894
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-09-24 15:16:46 +0900

    chore: skip data uri by load fallback plugin for native data uri handling

--- packages/vite/src/node/plugins/loadFallback.ts
+++ packages/vite/src/node/plugins/loadFallback.ts
@@ -1,24 +1,31 @@
 import fsp from 'node:fs/promises'
+import type { RolldownPlugin } from 'rolldown'
 import { cleanUrl } from '../../shared/utils'
-import type { Plugin } from '../plugin'
 
 /**
  * A plugin to provide build load fallback for arbitrary request with queries.
  */
-export function buildLoadFallbackPlugin(): Plugin {
+export function buildLoadFallbackPlugin(): RolldownPlugin {
   return {
     name: 'vite:load-fallback',
-    async load(id) {
-      try {
-        const cleanedId = cleanUrl(id)
-        const content = await fsp.readFile(cleanedId, 'utf-8')
-        this.addWatchFile(cleanedId)
-        return content
-      } catch {
-        const content = await fsp.readFile(id, 'utf-8')
-        this.addWatchFile(id)
-        return content
-      }
+    load: {
+      filter: {
+        id: {
+          exclude: [/^data:/],
+        },
+      },
+      async handler(id) {
+        try {
+          const cleanedId = cleanUrl(id)
+          const content = await fsp.readFile(cleanedId, 'utf-8')
+          this.addWatchFile(cleanedId)
+          return content
+        } catch {
+          const content = await fsp.readFile(id, 'utf-8')
+          this.addWatchFile(id)
+          return content
+        }
+      },
     },
   }
 }
