9ad08d121f5236cf48066af7d0e3377dac3df768
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-09-24 13:47:21 +0900

    chore: bump rolldown

--- packages/vite/src/node/server/pluginContainer.ts
+++ packages/vite/src/node/server/pluginContainer.ts
@@ -73,6 +73,7 @@ import {
   normalizePath,
   numberToPos,
   prettifyUrl,
+  rolldownVersion,
   rollupVersion,
   timeFrom,
 } from '../utils'
@@ -178,6 +179,7 @@ class EnvironmentPluginContainer {
     this.minimalContext = {
       meta: {
         rollupVersion,
+        rolldownVersion,
         watchMode: true,
       },
       debug: noop,
@@ -543,7 +545,7 @@ class PluginContext implements Omit<RollupPluginContext, 'cache'> {
   _activeId: string | null = null
   _activeCode: string | null = null
   _resolveSkips?: Set<Plugin>
-  // meta: RollupPluginContext['meta']
+  meta: RollupPluginContext['meta']
   environment: Environment
 
   constructor(
@@ -551,7 +553,7 @@ class PluginContext implements Omit<RollupPluginContext, 'cache'> {
     public _container: EnvironmentPluginContainer,
   ) {
     this.environment = this._container.environment
-    // this.meta = this._container.minimalContext.meta
+    this.meta = this._container.minimalContext.meta
   }
 
   parse(code: string, opts: any) {
