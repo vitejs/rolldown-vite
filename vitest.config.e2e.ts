a4034bd526d39c9bc7a2d9f277ed1f5c8410e808
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-10-01 16:06:31 +0900

    chore: use advancedChunks instead of manualChunks

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -18,12 +18,10 @@ export default defineConfig({
       ...(isBuild
         ? [
             './playground/backend-integration/**/*.spec.[tj]s', // https://github.com/rolldown/rolldown/issues/2207
-            './playground/css-codesplit/**/*.spec.[tj]s', // manualChunks
             './playground/css-dynamic-import/**/*.spec.[tj]s', // https://github.com/rolldown/rolldown/issues/1842
             './playground/dynamic-import/**/*.spec.[tj]s', // https://github.com/rolldown/rolldown/issues/1842, https://github.com/rolldown/rolldown/issues/1843
             './playground/external/**/*.spec.[tj]s', // https://github.com/rolldown/rolldown/issues/2041
             './playground/glob-import/**/*.spec.[tj]s', // remove empty CSS generate chunk https://github.com/rolldown/rolldown/issues/1842
-            './playground/js-sourcemap/**/*.spec.[tj]s', // manualChunks
             './playground/lib/**/*.spec.[tj]s', // umd format
             './playground/object-hooks/**/*.spec.[tj]s', // object hook sequential
             './playground/optimize-deps/**/*.spec.[tj]s', // https://github.com/rolldown/rolldown/issues/2031
