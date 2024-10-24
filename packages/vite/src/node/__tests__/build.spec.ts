42b959c250771b535ff61fd7d8e109153547b4be
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-10-24 16:30:02 +0900

    chore: update some tests that relies on rollup's behavior

--- packages/vite/src/node/__tests__/build.spec.ts
+++ packages/vite/src/node/__tests__/build.spec.ts
@@ -865,7 +865,7 @@ test.for([true, false])(
   },
 )
 
-test('adjust worker build error for worker.format', async () => {
+test.skip('adjust worker build error for worker.format', async () => {
   try {
     await build({
       root: resolve(__dirname, 'fixtures/worker-dynamic'),
