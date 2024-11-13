10157fdb8e736c7cd59ee5dc19cb719ef55a7123
翠 / green <green@sapphi.red>
2024-11-13 21:22:33 +0900

    feat: enable lib mode tests (#64)

--- packages/vite/src/node/build.ts
+++ packages/vite/src/node/build.ts
@@ -314,7 +314,7 @@ export interface LibraryOptions {
   cssFileName?: string
 }
 
-export type LibraryFormats = 'es' | 'cjs' | 'iife' // | 'umd' | 'system'
+export type LibraryFormats = 'es' | 'cjs' | 'iife' | 'umd' // | 'system'
 
 export interface ModulePreloadOptions {
   /**
@@ -773,7 +773,7 @@ async function buildEnvironment(
           ? `[name].[ext]`
           : path.posix.join(options.assetsDir, `[name]-[hash].[ext]`),
         inlineDynamicImports:
-          /* output.format === 'umd' || */ output.format === 'iife',
+          output.format === 'umd' || output.format === 'iife',
         ...output,
       }
     }
@@ -931,7 +931,7 @@ function resolveOutputJsExtension(
   type: string = 'commonjs',
 ): JsExt {
   if (type === 'module') {
-    return format === 'cjs' /* || format === 'umd' */ ? 'cjs' : 'js'
+    return format === 'cjs' || format === 'umd' ? 'cjs' : 'js'
   } else {
     return format === 'es' ? 'mjs' : 'js'
   }
@@ -981,10 +981,10 @@ export function resolveBuildOutputs(
       Object.values(libOptions.entry).length > 1
     const libFormats =
       libOptions.formats ||
-      (libHasMultipleEntries ? ['es', 'cjs'] : ['es' /* , 'umd' */])
+      (libHasMultipleEntries ? ['es', 'cjs'] : ['es', 'umd'])
 
     if (!Array.isArray(outputs)) {
-      if (/* libFormats.includes('umd') || */ libFormats.includes('iife')) {
+      if (libFormats.includes('umd') || libFormats.includes('iife')) {
         if (libHasMultipleEntries) {
           throw new Error(
             'Multiple entry points are not supported when output formats include "umd" or "iife".',
@@ -1012,7 +1012,7 @@ export function resolveBuildOutputs(
 
     outputs.forEach((output) => {
       if (
-        /* output.format === 'umd' || */ output.format === 'iife' &&
+        (output.format === 'umd' || output.format === 'iife') &&
         !output.name
       ) {
         throw new Error(
