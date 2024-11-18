88575485268da584a774de2fd64e53cf1e2a04ea
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-11-18 20:19:54 +0900

    chore: reduce diff

--- packages/vite/src/node/plugins/json.ts
+++ packages/vite/src/node/plugins/json.ts
@@ -71,7 +71,7 @@ export function jsonPlugin(
             return {
               code,
               map: { mappings: '' },
-              moduleType: 'js', // TODO: remove later when not needed
+              moduleType: 'js',
             }
           }
 
@@ -90,7 +90,7 @@ export function jsonPlugin(
             return {
               code: `export default JSON.parse(${JSON.stringify(json)})`,
               map: { mappings: '' },
-              moduleType: 'js', // TODO: remove later when not needed
+              moduleType: 'js',
             }
           }
         }
@@ -101,7 +101,7 @@ export function jsonPlugin(
             namedExports: options.namedExports,
           }),
           map: { mappings: '' },
-          moduleType: 'js', // TODO: remove later when not needed
+          moduleType: 'js',
         }
       } catch (e) {
         const position = extractJsonErrorPosition(e.message, json.length)
