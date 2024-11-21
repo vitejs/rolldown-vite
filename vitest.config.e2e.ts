51a309691ab2de63d0e6be7b1f7a128fc8d3d597
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-11-21 12:48:22 +0900

    chore: add workaround and make environment-react-ssr playground pass

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -17,7 +17,6 @@ export default defineConfig({
       './playground/legacy/**/*.spec.[tj]s', // system format
       ...(isBuild
         ? [
-            './playground/environment-react-ssr/**/*.spec.[tj]s', // needs investigation
             './playground/external/**/*.spec.[tj]s', // https://github.com/rolldown/rolldown/issues/2041
             './playground/object-hooks/**/*.spec.[tj]s', // object hook sequential
             './playground/optimize-deps/**/*.spec.[tj]s', // https://github.com/rolldown/rolldown/issues/2031
