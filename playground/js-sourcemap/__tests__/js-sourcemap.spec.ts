ccc5f16bdd40fcfa5026a2e58bff4241f1505f8c
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-01-16 17:39:48 +0900

    chore: tweak snapshots

--- playground/js-sourcemap/__tests__/js-sourcemap.spec.ts
+++ playground/js-sourcemap/__tests__/js-sourcemap.spec.ts
@@ -143,7 +143,7 @@ describe.runIf(isBuild)('build tests', () => {
       {
         "debugId": "00000000-0000-0000-0000-000000000000",
         "ignoreList": [],
-        "mappings": ";kpCAAA,OAAO,6BAAuB,wBAE9B,QAAQ,IAAI,wBAAuB",
+        "mappings": ";ynCAEA,kBAFA,OAAO,6BAAuB,wBAE9B,QAAQ,IAAI,wBAAuB",
         "sources": [
           "../../after-preload-dynamic.js",
         ],
