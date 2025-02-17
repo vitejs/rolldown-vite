a2d46a767e696d526cda9a11cd0361c2019ee2e3
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-02-17 12:57:19 +0900

    chore: fix parseAst rolldown compat

--- packages/vite/src/node/parseAst.ts
+++ packages/vite/src/node/parseAst.ts
@@ -1,51 +1,18 @@
 import {
-  type ParseResult,
   parseAst as rolldownParseAst,
   parseAstAsync as rolldownParseAstAsync,
 } from 'rolldown/parseAst'
 
 // TODO: move this compat layer to Rolldown
 
-const parseAstGeneric = (
-  code: string,
-  opts?: any,
-  filename?: string,
-): ParseResult => {
-  const result = rolldownParseAst(filename ?? 'file.js', code, {
-    sourceType: 'module',
-    lang: 'js',
-    preserveParens: false,
-    convertSpanUtf16: true,
-    ...opts,
-  })
-  if (result.errors.length > 0) {
-    throw new AggregateError(result.errors)
-  }
-
-  return result
-}
-
-export const parseAstGenericAsync = async (
-  code: string,
-  opts?: any,
-  filename?: string,
-): Promise<ParseResult> => {
-  const result = await rolldownParseAstAsync(filename ?? 'file.js', code, {
-    sourceType: 'module',
-    lang: 'js',
-    preserveParens: false,
-    convertSpanUtf16: true,
-    ...opts,
-  })
-  if (result.errors.length > 0) {
-    throw new AggregateError(result.errors)
-  }
-
-  return result
-}
-
 export const parseAst = (code: string, opts?: any, filename?: string): any => {
-  return parseAstGeneric(code, opts, filename).program
+  return rolldownParseAst(filename ?? 'file.js', code, {
+    sourceType: 'module',
+    lang: 'js',
+    preserveParens: false,
+    convertSpanUtf16: true,
+    ...opts,
+  })
 }
 
 export const parseAstAsync = async (
@@ -53,6 +20,11 @@ export const parseAstAsync = async (
   opts?: any,
   filename?: string,
 ): Promise<any> => {
-  const result = await parseAstGenericAsync(code, opts, filename)
-  return result.program
+  return await rolldownParseAstAsync(filename ?? 'file.js', code, {
+    sourceType: 'module',
+    lang: 'js',
+    preserveParens: false,
+    convertSpanUtf16: true,
+    ...opts,
+  })
 }
