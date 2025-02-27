dfef4161d897f7543873e514b4a4c588623db931
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-02-27 14:54:41 +0900

    feat: filter loadFallback plugin

--- packages/vite/src/node/plugins/loadFallback.ts
+++ packages/vite/src/node/plugins/loadFallback.ts
@@ -11,6 +11,7 @@ export function buildLoadFallbackPlugin(): RolldownPlugin {
     load: {
       filter: {
         id: {
+          include: /\?|#/,
           exclude: [/^data:/],
         },
       },
