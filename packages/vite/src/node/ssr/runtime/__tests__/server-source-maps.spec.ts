3607409414bcf4c1027d8b11e4ae83bb8e86ca7b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-03-12 11:48:04 +0900

    test: update test and note https://github.com/vitejs/vite/issues/19627

--- packages/vite/src/node/ssr/runtime/__tests__/server-source-maps.spec.ts
+++ packages/vite/src/node/ssr/runtime/__tests__/server-source-maps.spec.ts
@@ -35,8 +35,8 @@ describe('module runner initialization', async () => {
     const topLevelError = await getError(() =>
       runner.import('/fixtures/has-error.js'),
     )
-    expect(serializeStack(server, topLevelError)).toMatchInlineSnapshot(
-      `"    at <root>/fixtures/has-error.js:2:17"`,
+    expect(serializeStack(server, topLevelError)).toBe(
+      '    at <root>/fixtures/has-error.js:2:7',
     )
 
     const methodError = await getError(async () => {
