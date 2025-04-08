d168aad82e46198556312af709c6c0755868c145
underfin <likui.underfin@gmail.com>
2025-04-08 17:16:38 +0800

    fix: disable rolldown minify at devlopment build

--- packages/vite/src/node/build.ts
+++ packages/vite/src/node/build.ts
@@ -585,7 +585,7 @@ async function buildEnvironment(
   environment: BuildEnvironment,
   server?: ViteDevServer
 ): Promise<RolldownOutput | RolldownOutput[] | RolldownWatcher> {
-  const { root, packageCache } = environment.config
+  const { root, packageCache, mode } = environment.config
   const options = environment.config.build
   const libOptions = options.lib
   const { logger } = environment
@@ -818,11 +818,12 @@ async function buildEnvironment(
           output.format === 'iife' ||
           (isSsrTargetWebworkerEnvironment &&
             (typeof input === 'string' || Object.keys(input).length === 1)),
-        minify:
+        minify: mode === 'production' ?
           options.minify === 'oxc'
             ? true
             : options.minify === false
               ? 'dce-only'
+              : false
               : false,
         ...output,
       }
