02942289a4b4bbe1db09107f9082d9fb87ab29d5
underfin <likui.underfin@gmail.com>
2025-04-21 16:42:52 +0800

    chore: fix createBuilder typing

--- packages/vite/src/node/build.ts
+++ packages/vite/src/node/build.ts
@@ -1752,7 +1752,7 @@ export type ResolvedBuilderOptions = Required<BuilderOptions>
 export async function createBuilder(
   inlineConfig: InlineConfig = {},
   useLegacyBuilder: null | boolean = false,
-  command: 'build' | 'serve',
+  command: 'build' | 'serve' = 'build',
 ): Promise<ViteBuilder> {
   const patchConfig = (resolved: ResolvedConfig) => {
     if (!(useLegacyBuilder ?? !resolved.builder)) return
