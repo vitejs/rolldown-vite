5dcd721f363f6f03cdbd89d4f646c886af64a4b0
underfin <likui.underfin@gmail.com>
2025-04-21 17:02:33 +0800

    fix: __FULL_BUNDLE_MODE__ replace

--- packages/vite/src/node/plugins/clientInjections.ts
+++ packages/vite/src/node/plugins/clientInjections.ts
@@ -93,7 +93,7 @@ export function clientInjectionsPlugin(config: ResolvedConfig): Plugin {
           .replace(`__HMR_ENABLE_OVERLAY__`, hmrEnableOverlayReplacement)
           .replace(`__HMR_CONFIG_NAME__`, hmrConfigNameReplacement)
           .replace(`__WS_TOKEN__`, wsTokenReplacement)
-          .replace(
+          .replaceAll(
             `__FULL_BUNDLE_MODE__`,
             escapeReplacement(config.experimental.fullBundleMode || false),
           )
@@ -187,7 +187,7 @@ export async function getHmrImplement(config: ResolvedConfig): Promise<string> {
     .replace(`__HMR_ENABLE_OVERLAY__`, hmrEnableOverlayReplacement)
     .replace(`__HMR_CONFIG_NAME__`, hmrConfigNameReplacement)
     .replace(`__WS_TOKEN__`, wsTokenReplacement)
-    .replace(
+    .replaceAll(
       `__FULL_BUNDLE_MODE__`,
       escapeReplacement(config.experimental.fullBundleMode || false),
     )
