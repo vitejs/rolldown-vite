076873610ead8e4b281fe30efae49a33ebaa8684
underfin <likui.underfin@gmail.com>
2025-04-21 16:53:17 +0800

    chore: fix cli fullBundleMode

--- packages/vite/src/node/build.ts
+++ packages/vite/src/node/build.ts
@@ -587,7 +587,7 @@ async function buildEnvironment(
   environment: BuildEnvironment,
   server?: ViteDevServer
 ): Promise<RolldownOutput | RolldownOutput[] | RolldownWatcher> {
-  const { root, packageCache } = environment.config
+  const { root, packageCache, experimental, command } = environment.config
   const options = environment.config.build
   const libOptions = options.lib
   const { logger } = environment
@@ -821,11 +821,13 @@ async function buildEnvironment(
           (isSsrTargetWebworkerEnvironment &&
             (typeof input === 'string' || Object.keys(input).length === 1)),
         minify:
-          options.minify === 'oxc'
-            ? true
-            : options.minify === false
-              ? 'dce-only'
-              : false,
+          experimental.fullBundleMode && command === 'serve'
+            ? false
+            : options.minify === 'oxc'
+              ? true
+              : options.minify === false
+                ? 'dce-only'
+                : false,
         ...output,
       }
     }
