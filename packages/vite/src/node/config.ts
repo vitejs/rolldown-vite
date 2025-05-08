ec9bf2ea54054f3b75e3e313bc695811e3a8989f
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-05-08 22:05:43 +0900

    ci: run tests with native resolver

--- packages/vite/src/node/config.ts
+++ packages/vite/src/node/config.ts
@@ -714,7 +714,9 @@ export const configDefaults = Object.freeze({
     renderBuiltUrl: undefined,
     hmrPartialAccept: false,
     skipSsrTransform: false,
-    enableNativePlugin: false,
+    enableNativePlugin: process.env._VITE_TEST_NATIVE_PLUGIN
+      ? 'resolver'
+      : false,
   },
   future: {
     removePluginHookHandleHotUpdate: undefined,
