9c7f704de91cbc0d62e867795170a94802ec0046
underfin <likui.underfin@gmail.com>
2025-05-13 16:14:23 +0800

    fix: react plugin get module exports

--- packages/vite/src/shared/hmr.ts
+++ packages/vite/src/shared/hmr.ts
@@ -155,13 +155,13 @@ export class HMRContext implements ViteHotContext {
     this.hmrClient.send({ type: 'custom', event, data })
   }
 
-  async getExports(): Promise<unknown> {
+  async getExports(url: string): Promise<unknown> {
     return this.fullBundleMode
       ? Promise.resolve().then(() =>
           // @ts-expect-error __rolldown_runtime__
           __rolldown_runtime__.loadExports(this.ownerPath),
         )
-      : import(this.ownerPath)
+      : import(url)
   }
 
   private acceptDeps(
