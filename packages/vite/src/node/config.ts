9af37a879072cb5d8bc94b715b783dfcaddeab41
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-05-27 13:57:28 +0900

    feat: add `ResolvedConfig.isBundled`

--- packages/vite/src/node/config.ts
+++ packages/vite/src/node/config.ts
@@ -625,6 +625,8 @@ export interface ResolvedConfig
       cacheDir: string
       command: 'build' | 'serve'
       mode: string
+      /** `true` when build or full-bundle mode dev */
+      isBundled: boolean
       isWorker: boolean
       // in nested worker bundle to find the main config
       /** @internal */
@@ -1788,6 +1790,7 @@ export async function resolveConfig(
     cacheDir,
     command,
     mode,
+    isBundled: config.experimental?.fullBundleMode || isBuild,
     isWorker: false,
     mainConfig: null,
     bundleChain: [],
