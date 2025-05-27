c81be709886f67a6a1e2f10cd49006eb61062e45
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-05-27 12:42:18 +0900

    wip: full bundle dev env
    Co-Authored-By: underfin <likui.underfin@gmail.com>

--- packages/vite/src/node/build.ts
+++ packages/vite/src/node/build.ts
@@ -506,7 +506,7 @@ export async function resolveBuildPlugins(config: ResolvedConfig): Promise<{
     pre: [
       completeAmdWrapPlugin(),
       completeSystemWrapPlugin(),
-      ...(!config.isWorker ? [prepareOutDirPlugin()] : []),
+      ...(isBuild && !config.isWorker ? [prepareOutDirPlugin()] : []),
       perEnvironmentPlugin(
         'vite:rollup-options-plugins',
         async (environment) =>
@@ -522,8 +522,8 @@ export async function resolveBuildPlugins(config: ResolvedConfig): Promise<{
       ...(isBuild ? buildImportAnalysisPlugin(config) : []),
       ...(config.nativePluginEnabledLevel >= 1 ? [] : [buildOxcPlugin()]),
       ...(config.build.minify === 'esbuild' ? [buildEsbuildPlugin()] : []),
-      terserPlugin(config),
-      ...(!config.isWorker
+      ...(isBuild ? [terserPlugin(config)] : []),
+      ...(isBuild && !config.isWorker
         ? [
             licensePlugin(),
             manifestPlugin(config),
@@ -565,10 +565,10 @@ function resolveConfigToBuild(
   )
 }
 
-function resolveRolldownOptions(
+export function resolveRolldownOptions(
   environment: Environment,
   chunkMetadataMap: ChunkMetadataMap,
-) {
+): RolldownOptions {
   const { root, packageCache, build: options } = environment.config
   const libOptions = options.lib
   const { logger } = environment
@@ -885,7 +885,7 @@ async function buildEnvironment(
   }
 }
 
-function enhanceRollupError(e: RollupError) {
+export function enhanceRollupError(e: RollupError): void {
   const stackOnly = extractStack(e)
 
   let msg = colors.red((e.plugin ? `[${e.plugin}] ` : '') + e.message)
@@ -1051,7 +1051,7 @@ const dynamicImportWarningIgnoreList = [
   `statically analyzed`,
 ]
 
-function clearLine() {
+export function clearLine(): void {
   const tty = process.stdout.isTTY && !process.env.CI
   if (tty) {
     process.stdout.clearLine(0)
