bec9e6eadf7078b5d47e64ebcb18680fc30489e3
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-06-06 21:03:13 +0900

    wip: full bundle dev env

--- packages/vite/src/node/server/environment.ts
+++ packages/vite/src/node/server/environment.ts
@@ -236,7 +236,7 @@ export class DevEnvironment extends BaseEnvironment {
     }
   }
 
-  private invalidateModule(m: {
+  protected invalidateModule(m: {
     path: string
     message?: string
     firstInvalidatedBy: string
