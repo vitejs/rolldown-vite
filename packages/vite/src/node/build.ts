49afdc9c666d090b4d06e99ab30c6e5d8d7425de
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-06-16 17:51:50 +0900

    feat: disable minify by default in full bundle mode

--- packages/vite/src/node/build.ts
+++ packages/vite/src/node/build.ts
@@ -410,6 +410,7 @@ export function resolveBuildEnvironmentOptions(
   raw: BuildEnvironmentOptions,
   logger: Logger,
   consumer: 'client' | 'server' | undefined,
+  isFullBundledDev: boolean,
 ): ResolvedBuildEnvironmentOptions {
   const deprecatedPolyfillModulePreload = raw.polyfillModulePreload
   const { polyfillModulePreload, ...rest } = raw
@@ -430,7 +431,7 @@ export function resolveBuildEnvironmentOptions(
     {
       ..._buildEnvironmentOptionsDefaults,
       cssCodeSplit: !raw.lib,
-      minify: consumer === 'server' ? false : 'oxc',
+      minify: consumer === 'server' || isFullBundledDev ? false : 'oxc',
       rollupOptions: {},
       rolldownOptions: undefined,
       ssr: consumer === 'server',
--- packages/vite/src/node/config.ts
+++ packages/vite/src/node/config.ts
@@ -846,6 +846,7 @@ function resolveEnvironmentOptions(
   forceOptimizeDeps: boolean | undefined,
   logger: Logger,
   environmentName: string,
+  isFullBundledDev: boolean,
   // Backward compatibility
   isSsrTargetWebworkerSet?: boolean,
   preTransformRequests?: boolean,
@@ -908,6 +909,7 @@ function resolveEnvironmentOptions(
       options.build ?? {},
       logger,
       consumer,
+      isFullBundledDev,
     ),
     plugins: undefined!, // to be resolved later
     // will be set by `setOptimizeDepsPluginNames` later
@@ -1496,6 +1498,9 @@ export async function resolveConfig(
     config.ssr?.target === 'webworker',
   )
 
+  const isFullBundledDev =
+    command === 'serve' && !!config.experimental?.fullBundleMode
+
   // Backward compatibility: merge config.environments.client.resolve back into config.resolve
   config.resolve ??= {}
   config.resolve.conditions = config.environments.client.resolve?.conditions
@@ -1512,6 +1517,7 @@ export async function resolveConfig(
       inlineConfig.forceOptimizeDeps,
       logger,
       environmentName,
+      isFullBundledDev,
       config.ssr?.target === 'webworker',
       config.server?.preTransformRequests,
     )
@@ -1535,6 +1541,7 @@ export async function resolveConfig(
     config.build ?? {},
     logger,
     undefined,
+    isFullBundledDev,
   )
 
   // Backward compatibility: merge config.environments.ssr back into config.ssr
