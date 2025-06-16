581ee1870c55364c6338dce5bdcc7eceb6bf1233
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-06-16 17:40:17 +0900

    Revert "feat: disable minify by default in development"
    This reverts commit 2af81b7c4aaead8a022a40d7368aaaf2c931129f.

--- packages/vite/src/node/build.ts
+++ packages/vite/src/node/build.ts
@@ -396,7 +396,6 @@ export function resolveBuildEnvironmentOptions(
   raw: BuildEnvironmentOptions,
   logger: Logger,
   consumer: 'client' | 'server' | undefined,
-  isProduction: boolean,
 ): ResolvedBuildEnvironmentOptions {
   const deprecatedPolyfillModulePreload = raw.polyfillModulePreload
   const { polyfillModulePreload, ...rest } = raw
@@ -417,7 +416,7 @@ export function resolveBuildEnvironmentOptions(
     {
       ...buildEnvironmentOptionsDefaults,
       cssCodeSplit: !raw.lib,
-      minify: !isProduction || consumer === 'server' ? false : 'oxc',
+      minify: consumer === 'server' ? false : 'oxc',
       rollupOptions: {
         platform: consumer === 'server' ? 'node' : 'browser',
       },
