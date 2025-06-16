083204c84ca15643fd33f37e28513c0493b9e3ff
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-06-16 17:51:50 +0900

    feat: disable minify by default in full bundle mode

--- packages/vite/src/node/config.ts
+++ packages/vite/src/node/config.ts
@@ -818,6 +818,7 @@ function resolveEnvironmentOptions(
   forceOptimizeDeps: boolean | undefined,
   logger: Logger,
   environmentName: string,
+  isFullBundledDev: boolean,
   // Backward compatibility
   isSsrTargetWebworkerSet?: boolean,
   preTransformRequests?: boolean,
@@ -880,6 +881,7 @@ function resolveEnvironmentOptions(
       options.build ?? {},
       logger,
       consumer,
+      isFullBundledDev,
     ),
     plugins: undefined!, // to be resolved later
     // will be set by `setOptimizeDepsPluginNames` later
@@ -1449,6 +1451,9 @@ export async function resolveConfig(
     config.ssr?.target === 'webworker',
   )
 
+  const isFullBundledDev =
+    command === 'serve' && !!config.experimental?.fullBundleMode
+
   // Backward compatibility: merge config.environments.client.resolve back into config.resolve
   config.resolve ??= {}
   config.resolve.conditions = config.environments.client.resolve?.conditions
@@ -1465,6 +1470,7 @@ export async function resolveConfig(
       inlineConfig.forceOptimizeDeps,
       logger,
       environmentName,
+      isFullBundledDev,
       config.ssr?.target === 'webworker',
       config.server?.preTransformRequests,
     )
@@ -1488,6 +1494,7 @@ export async function resolveConfig(
     config.build ?? {},
     logger,
     undefined,
+    isFullBundledDev,
   )
 
   // Backward compatibility: merge config.environments.ssr back into config.ssr
