1506d12afccab71df841046cb758203191050916
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-06-16 18:09:27 +0900

    feat: disable buildImportAnalysisPlugin for full bundle mode

--- packages/vite/src/node/build.ts
+++ packages/vite/src/node/build.ts
@@ -489,6 +489,7 @@ export async function resolveBuildPlugins(config: ResolvedConfig): Promise<{
   pre: Plugin[]
   post: Plugin[]
 }> {
+  const isBuild = config.command === 'build'
   return {
     pre: [
       completeSystemWrapPlugin(),
@@ -505,7 +506,7 @@ export async function resolveBuildPlugins(config: ResolvedConfig): Promise<{
       ...(config.isWorker ? [webWorkerPostPlugin(config)] : []),
     ],
     post: [
-      ...buildImportAnalysisPlugin(config),
+      ...(isBuild ? buildImportAnalysisPlugin(config) : []),
       ...(config.nativePluginEnabledLevel >= 1 ? [] : [buildOxcPlugin()]),
       ...(config.build.minify === 'esbuild' ? [buildEsbuildPlugin()] : []),
       terserPlugin(config),
