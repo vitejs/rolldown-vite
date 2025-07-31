613590e906a1173237c1ad9d252da91538817c76
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-07-31 13:00:12 +0900

    wip: skip optimizerResolvePlugin

--- packages/vite/src/node/plugins/resolve.ts
+++ packages/vite/src/node/plugins/resolve.ts
@@ -236,7 +236,7 @@ export function oxcResolvePlugin(
   overrideEnvConfig: (ResolvedConfig & ResolvedEnvironmentOptions) | undefined,
 ): Plugin[] {
   return [
-    ...(!resolveOptions.isBuild
+    ...(resolveOptions.optimizeDeps && !resolveOptions.isBuild
       ? [optimizerResolvePlugin(resolveOptions)]
       : []),
     ...perEnvironmentOrWorkerPlugin(
@@ -395,6 +395,12 @@ function optimizerResolvePlugin(
 
   return {
     name: 'vite:resolve-dev',
+    applyToEnvironment(environment) {
+      return (
+        !environment.config.experimental.fullBundleMode &&
+        !isDepOptimizationDisabled(environment.config.optimizeDeps)
+      )
+    },
     resolveId: {
       filter: {
         id: {
