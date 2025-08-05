458e75ca5d159140d17e61fdd63766486b86620c
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-08-05 20:39:42 +0900

    wip: disable renderBuiltUrl in dev

--- packages/vite/src/node/config.ts
+++ packages/vite/src/node/config.ts
@@ -1740,6 +1740,15 @@ export async function resolveConfig(
     )
   }
 
+  const experimental = mergeWithDefaults(
+    configDefaults.experimental,
+    config.experimental ?? {},
+  )
+  if (command === 'serve' && experimental.fullBundleMode) {
+    // full bundle mode does not support experimental.renderBuiltUrl
+    experimental.renderBuiltUrl = undefined
+  }
+
   resolved = {
     configFile: configFile ? normalizePath(configFile) : undefined,
     configFileDependencies: configFileDependencies.map((name) =>
@@ -1802,10 +1811,7 @@ export async function resolveConfig(
     packageCache,
     worker: resolvedWorkerOptions,
     appType: config.appType ?? 'spa',
-    experimental: mergeWithDefaults(
-      configDefaults.experimental,
-      config.experimental ?? {},
-    ),
+    experimental,
     future:
       config.future === 'warn'
         ? ({
